23edc94d29cf562552e29d08d6703e8bb1ee12e4
anusha-poudel <anusha0227@xavier.edu.np>
2026-09-19 20:37:39 +0545

    feat: add score grading classifier

---
+++ js_assignment/Q2.gradeClassifier.js
@@ -0,0 +1,18 @@
+function getGrade(score){
+    if(score >= 90){
+        console.log('A');
+    }
+    else if(score >= 80 && score>=89){
+        console.log('B');
+    }
+    else if(score >= 70 && score>=79){
+        console.log('C');
+    }
+    else if(score >= 60 && score>=69){
+        console.log('B');
+    }
+    else{
+        console.log('F');
+    }
+}
+getGrade(91);
