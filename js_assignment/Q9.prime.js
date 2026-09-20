0d4ce8e00e421ceae7599d4ea1d92d882c5aa422
anusha-poudel <anusha0227@xavier.edu.np>
2026-09-20 21:30:02 +0545

    feat: add prime number checker

---
+++ js_assignment/Q9.prime.js
@@ -0,0 +1,13 @@
+function isPrime(n){
+    if(n<=1) return false;
+
+    else if(n%2===0) return false;
+    
+    for(let i=2; i<n; i++){
+        if(n%i===0){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPrime(71));
