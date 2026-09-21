fda3da92f61d0d7ae6e0fc3584961cdde7722fc7
anusha-poudel <anusha0227@xavier.edu.np>
2026-09-21 21:18:20 +0545

    feat: add extract from object

---
+++ js_assignment/part2/Q14.extract.js
@@ -0,0 +1,7 @@
+function formatUser(user){
+    const {name, email, age} = user;
+    return `${name} (${email})`;
+}
+
+let user= formatUser({name:'Anusha', email:'anusha@gmail.com', age: 20});
+console.log(user);
